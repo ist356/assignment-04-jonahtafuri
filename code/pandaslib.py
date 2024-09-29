b8d59295304f0c4169c830a386d0c47e566336ab
jttafuri@syr.edu <jttafuri@syr.edu>
2024-09-29 13:05:10 -0400

    completed pandaslib section

--- code/pandaslib.py
+++ code/pandaslib.py
@@ -3,28 +3,40 @@ A Library of useful pandas helper functions
 SOLUTION FILE!!!!
 '''
 import pandas as pd
+import requests
+
+
 
 def get_column_names(df : pd.DataFrame) -> list[str]:
     '''
     Get all column names of a pandas dataframe df
     Returns the names as a list of string
     '''
-    pass # todo: replace this line and add your code
-
+    names = df.columns.tolist()
+    return names
 
 def get_columns_of_type(df : pd.DataFrame, numpy_type: any) -> list[str]:
     '''
     Return the column names of a pandas dataframe only when 
     the values in the column match the numpy_type
     '''
-    pass # todo: replace this line and add your code
+    names = df.columns.tolist()
+    types = df.dtypes.tolist()
+    my_dict = dict(zip(names, types))
+    # print("TEST", my_dict)
+    filtered_dict = {key: value for key, value in my_dict.items() if value == numpy_type}
+    col_names_filters = list(filtered_dict.keys())
+    # print("TEST2", col_names_filters)
+    return(col_names_filters)
 
 
 def get_unique_values(df : pd.DataFrame, column_name: str) -> list:
     '''
     Get a list of unique values of a column in a pandas dataframe
     '''
-    pass # todo: replace this line and add your code
+    col = df[column_name]
+    values = col.unique()
+    return values 
 
 def get_file_extension(file_path : str) -> str:
     '''
@@ -34,7 +46,11 @@ def get_file_extension(file_path : str) -> str:
     'countries.json' -> 'json'
 
     '''
-    pass # todo: replace this line and add your code
+    filepath_list = file_path.split(sep="/")
+    final_entry = filepath_list[-1]
+    final_entry = final_entry.split(sep=".")
+    file_type = final_entry[-1]
+    return file_type
 
 def load_file(file_path: str, ext: str) -> pd.DataFrame:
     '''
@@ -44,7 +60,17 @@ def load_file(file_path: str, ext: str) -> pd.DataFrame:
     - when csv assume first row is header
     - when json assume record-oriented data
     '''
-    pass # todo: replace this line and add your code
+    file_type = get_file_extension(file_path)
+    if file_type == 'csv':
+        file = pd.read_csv(file_path, header=0)
+    elif file_type == 'json':
+        file = pd.read_json(file_path, orient='records')
+    elif file_type == 'xlsx':
+        file = pd.read_excel(file_path)
+    else:
+        raise ValueError(f"unsupported file type {file_type}")
+    
+    return file
 
 if __name__ == '__main__':
     df = pd.DataFrame({ 
@@ -63,7 +89,8 @@ if __name__ == '__main__':
     print(f"Float64 Columns: {cols}")
     unique = get_unique_values(df, 'state')
     print(f"Unique States: {unique}")
-
+    print("Should be 'csv':", get_file_extension("/some/file/data.csv"))
+    print("Should be 'json':", get_file_extension("/some/file/data.json"))
 
 
 
--- code/unibrow.py
+++ code/unibrow.py
@@ -1,6 +1,17 @@
 '''
 Solution unibrow.py
 '''
+# It consists of 3 inputs:
+
+# - upload a file in Excel (XLSX), Comma-Separared, with Header (CSV), or Row-Oriented Json (JSON) into a dataframe
+# - select which columns to display from the dataframe
+# - build a fiter on the dataframe based on a text column and one of the values in the column.
+
+# And 2 outputs:
+
+# - the dataframe with column / row filters applied.
+# - the describe of the dataframe (to see statistics for the numerical columns)
+
 import pandas as pd
 import streamlit as st
 import pandaslib as pl
